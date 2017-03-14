8cc4b889df2f7d7b7aef5aab5ec337d2193c65bd
Samy Kebaish <skebaish1992@gmail.com>
2017-03-13 22:42:45 -0700

    Server working to default path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-
+var path    = require("path");
 var app = express();
 
 
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+// app.set('view engine', 'jade');
 
 // require('./config/middleware.js')(app, express);
 // require('./config/routes.js')(app, express);
@@ -19,6 +20,11 @@ var port = process.env.PORT || 1337;
 app.listen(port);
 
 console.log("Now listening to port" + port);
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../public/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
