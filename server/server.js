a44c0ff941fb08ae923bf0c9a6dc26e05d1141fc
Samy Kebaish <skebaish1992@gmail.com>
2017-03-13 22:03:52 -0700

    Adding files

--- server/server.js
+++ server/server.js
@@ -3,13 +3,22 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/shortly');
 
-require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// require('./config/middleware.js')(app, express);
+// require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(8000);
+var port = process.env.PORT || 1337;
+
+app.listen(port);
+
+console.log("Now listening to port" + port);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
