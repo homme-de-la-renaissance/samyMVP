9105bd8d02508c0d7fcf1155272d974c3cac3b63
Samy Kebaish <skebaish1992@gmail.com>
2017-03-13 18:07:06 -0700

    Added primitive dynamic query handling

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,28 @@
-var subjectivityApp = angular.module('subjectivityApp', []);
+var subjectivityApp = angular.module('subjectivityApp', []).controller('userResponseCtrl', function userResponseCtrl($scope) {
 
-subjectivityApp.controller('userResponseController', function userResponseController($scope) {
-  $scope.responses = [
-    {
-      name: 'Samy Kebaish',
-      subject: 'Biology',
-      grade: 99
-    },
-    {
-      name: 'Samy Kebaish',
-      subject: 'Chemistry',
-      grade: 98
-    }
+  $scope.responses = [];
+  // $scope.response.firstName = $scope.firstName;
+  // $scope.response.lastName = $scope.lastName;
+  // $scope.responses = [
+  // ];
 
-  ];
+  // $scope.submitForm = function(firstName, lastName) {
+  //   $scope.firstName = firstName;
+  //   $scope.lastName = lastName;
+
+  $scope.setSubjectValue = function($event) {
+    $scope.subject = $event.target.getAttribute('value');
+  };
+
+  $scope.setFeelingValue = function($event) {
+    $scope.feeling = $event.target.getAttribute('value');
+  };
+
+  $scope.addRow = function() {
+    $scope.responses.push({ 'name': $scope.firstName + ' ' + $scope.lastName, 'subject': $scope.subject, 'feeling': $scope.feeling});
+
+    console.log($scope.responses);
+
+  };
+
 });
